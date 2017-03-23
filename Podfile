9812aeb2152aa8bd3e6c32c0a92dff34661ddb71
Kurt Jensen <arborapps@gmail.com>
2017-03-23 18:09:10 -0400

    initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+
+target :RUN do
+    pod 'Fabric'
+    pod 'Crashlytics'
+  	pod 'GoogleMaps'
+  	pod 'RealmSwift'
+end
---
+++ RUN/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  RUN
+//
+//  Created by Kurt Jensen on 3/23/17.
+//  Copyright © 2017 Arbor Apps. All rights reserved.
+//
+
+import UIKit
+
+class Constants {
+    
+    static let googleMapsApiKey = "AIzaSyAE0_wLGmYZwsMqG7nF0VfCfmnXVU7oN1I"
+
+}
